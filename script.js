45503bb0dab9e500a8ea0aa7ec1186323d94823a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 09:28:44 -0300

    adicionando exercicio

---
+++ script.js
@@ -0,0 +1,21 @@
+let nome = "Theo"
+let idade = 21
+let status = "verdadeiro"
+let valorproduto = 10.50
+
+console.log("nome=",nome)
+console.log("idade=",idade)
+console.log(status)
+console.log("valor do produto=",valorproduto)
+
+function funca() { return (5 * 19); };
+console.log("multiplicacao 5*19",funca());
+
+function funcb() { return (5 / 5); };
+console.log("divisao 5/5",funcb());
+
+function funcc() { return (20 - 15); };
+console.log("subtracao 20-15",funcc());
+
+function funcd() { return (5 + 21); };
+console.log("soma 5+21",funcd());
